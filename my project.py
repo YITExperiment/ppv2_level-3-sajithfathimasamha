6cdcd2e35ecb1a0d2a2a2bdfffb14c9a9ce95307
sajithfathimasamha <136066245+sajithfathimasamha@users.noreply.github.com>
2023-06-25 14:52:35 +1000

    Add files via upload

---
+++ my project.py
@@ -0,0 +1,22 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/PPS/AppData/Local/Programs/Python/Python311/myfirst file.py
+Hello,World!
+>>> 
+= RESTART: C:/Users/PPS/AppData/Local/Programs/Python/Python311/myfirst file.py
+Hello,World!
+What is your name samha
+Hello  samha
+>>> age=12
+>>> print(age)
+12
+>>> weight=2
+>>> print(weight)
+56
+>>> 
+>>> Height=140cm
+SyntaxError: invalid decimal literal
+>>> Height=140
+>>> print(Height)
+154
